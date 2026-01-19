c7591be48c6d25b9d11f3fa2562bb0a810aa7ba0
Shikhar <shikharbhardwaj1993@gmail.com>
2026-01-19 22:37:36 +0530

    latest

--- workspace2026/browserstack/src/test/java/base/DriverFactory.java
+++ workspace2026/browserstack/src/test/java/base/DriverFactory.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.MutableCapabilities;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverFactory {
 
@@ -14,43 +16,48 @@ public class DriverFactory {
     public static WebDriver initDriver() {
 
         String runOn = System.getProperty("runOn", "local");
+        System.out.println("RUN MODE : " + runOn);
 
         try {
 
-            if (runOn.equalsIgnoreCase("local")) {
+            // ===== LOCAL =====
+            if ("local".equalsIgnoreCase(runOn)) {
                 driver = new ChromeDriver();
                 driver.manage().window().maximize();
+                System.out.println("Running tests locally");
                 return driver;
             }
 
-            if (runOn.equalsIgnoreCase("browserstack")) {
+            // ===== BROWSERSTACK =====
+            if ("browserstack".equalsIgnoreCase(runOn)) {
 
                 String username = System.getenv("BROWSERSTACK_USERNAME");
                 String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
 
                 if (username == null || accessKey == null) {
-                    throw new RuntimeException("BrowserStack credentials not found in environment variables");
+                    throw new RuntimeException("BrowserStack credentials not found in Jenkins environment variables");
                 }
 
                 MutableCapabilities caps = new MutableCapabilities();
                 caps.setCapability("browserName", "Chrome");
                 caps.setCapability("browserVersion", "latest");
 
-                MutableCapabilities bstackOptions = new MutableCapabilities();
-                bstackOptions.setCapability("os", "Windows");
-                bstackOptions.setCapability("osVersion", "11");
-                bstackOptions.setCapability("userName", username);
-                bstackOptions.setCapability("accessKey", accessKey);
-                bstackOptions.setCapability("projectName", "Selenium Java Framework");
-                bstackOptions.setCapability("buildName", "Jenkins Build");
-                bstackOptions.setCapability("sessionName", "Google Title Test");
+                Map<String, Object> bstackOptions = new HashMap<String, Object>();
+                bstackOptions.put("os", "Windows");
+                bstackOptions.put("osVersion", "11");
+                bstackOptions.put("projectName", "Selenium Java Framework");
+                bstackOptions.put("buildName", "Jenkins Build #" + System.getenv("BUILD_NUMBER"));
+                bstackOptions.put("sessionName", "Google Title Test");
 
                 caps.setCapability("bstack:options", bstackOptions);
 
-                driver = new RemoteWebDriver(
-                        new URL("https://hub.browserstack.com/wd/hub"),
-                        caps
-                );
+                String hubURL = "https://" + username + ":" + accessKey +
+                        "@hub-cloud.browserstack.com/wd/hub";
+
+                driver = new RemoteWebDriver(new URL(hubURL), caps);
+
+                System.out.println("Running tests on BrowserStack");
+                System.out.println("Hub URL: " + hubURL);
 
                 return driver;
             }
